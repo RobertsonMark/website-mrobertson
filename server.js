c8097b7d2e1cc9252b5c07c506e3a4663f0a7623
RobertsonMark <robertsonmark@outlook.com>
2018-07-01 12:08:26 -0700

    Fixed package.json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.port || 5000;
 
 app.get('/api/customers', (req, res) => {
     const customers = [
